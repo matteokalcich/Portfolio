8d6c170e81bc24c7c2e1ee5d289426f15ed39870
Matteo Kalcich <matteokalcich@gmail.com>
2025-07-04 15:35:26 +0200

    aggiunti skill

--- script.js
+++ script.js
@@ -6,3 +6,45 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
 });
+
+const track = document.querySelector('.carousel-track');
+const prevButton = document.querySelector('.carousel-button.prev');
+const nextButton = document.querySelector('.carousel-button.next');
+const items = document.querySelectorAll('.carousel-item');
+
+let index = 0;
+const itemWidth = items[0].offsetWidth + 16; // larghezza item + gap
+
+function updateCarousel() {
+  track.style.transform = `translateX(-${index * itemWidth}px)`;
+}
+
+nextButton.addEventListener('click', () => {
+  if (index < items.length - 3) { // mostra 3 per volta su desktop
+    index++;
+    updateCarousel();
+  }
+});
+
+prevButton.addEventListener('click', () => {
+  if (index > 0) {
+    index--;
+    updateCarousel();
+  }
+});
+
+// Swipe mobile
+let startX = 0;
+track.addEventListener('touchstart', (e) => {
+  startX = e.touches[0].clientX;
+});
+track.addEventListener('touchend', (e) => {
+  const endX = e.changedTouches[0].clientX;
+  if (startX - endX > 50 && index < items.length - 3) {
+    index++;
+    updateCarousel();
+  } else if (endX - startX > 50 && index > 0) {
+    index--;
+    updateCarousel();
+  }
+});
